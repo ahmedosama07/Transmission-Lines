50c351dd31b86dac0daaa62ce3fc50e9eb0b5aa4
Skinchizer <mezo214@gmail.com>
2023-04-22 13:01:18 +0200

    s

--- lineParameters.m
+++ lineParameters.m
@@ -1,10 +1,10 @@
-function [A, B, C, D] = lineParameters(length, R, C, L)
+function [A, B, C, D] = lineParameters(length, R, Cap, L)
 %lineParameters Summary of this function goes here
 %   function that calculates ABCD parameters of the transmission line
 
 length = length * 10^(-3);
 XL = 2 * pi * 50 * L;
-YC = 2 * pi * 50 * C * 10^(-6);
+YC = 2 * pi * 50 * Cap * 10^(-6);
 Z = complex(R, XL);
 Y = complex(0, YC);
 if length < 80

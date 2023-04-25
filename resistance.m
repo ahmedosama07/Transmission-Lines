694d41cb7869bcd5e497f0f69af4909111a86c98
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-04-25 19:34:22 +0200

    GUI stable

--- resistance.m
+++ resistance.m
@@ -3,5 +3,5 @@ function R = resistance(diameter, length, resistivity)
 %   function that calculates the resistance of the transmission line
 radius = (diameter / 2) * 10^(-2);          % convert radius to meters
 area = pi * radius^2 ;                      % conductor cross-section area
-R = resistivity * (length* 10^(3))/area;   % conductor resistance
+R = 1.11 * resistivity * (length* 10^(3))/area;   % conductor resistance
 end

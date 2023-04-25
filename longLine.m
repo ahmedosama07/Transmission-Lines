9929a86ebaf9979f5f7ff533e647fe8e6a62222c
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-04-25 17:40:26 +0200

    Update longLine.m

--- longLine.m
+++ longLine.m
@@ -7,8 +7,8 @@ fprintf("Cannot measure ABCD parameters.\n");
 fprintf("*The distributed nature of the parameters is considered.\n");
 fprintf("*The resistance, inductance, and capacitance of the line are not lumped.\n");
 
-fprintf("Line impedance: %.3f ∠ %.3f° Ω", abs(Z), rad2deg(andle(Z)));
-fprintf("Capacitive admitance: %.3f ∠ %.3f° ℧", abs(Y), rad2deg(andle(Y)));
+fprintf("Line impedance: %.3f ∠ %.3f° Ω", abs(Z), rad2deg(angle(Z)));
+fprintf("Capacitive admitance: %.3f ∠ %.3f° ℧", abs(Y), rad2deg(angle(Y)));
 
 [A, B, C, D] = struct('x', num2cell([NaN, NaN, NaN, NaN])).x;
 end
